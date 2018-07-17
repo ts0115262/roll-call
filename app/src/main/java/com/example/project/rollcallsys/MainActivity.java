77718dcd22f67c66a743e70b4efa842d9a299618
ts0115262 <jiang-hao@hotmail.com>
2018-07-17 17:35:20 +0800

    Added onClickListener

--- app/src/main/java/com/example/project/rollcallsys/MainActivity.java
+++ app/src/main/java/com/example/project/rollcallsys/MainActivity.java
@@ -2,14 +2,25 @@ package com.example.project.rollcallsys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 private Button button1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         button1.findViewById(R.id.hello);
+        button1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.hello:
+                Toast.makeText(this, "clicked", Toast.LENGTH_SHORT).show();
+        }
     }
 }
